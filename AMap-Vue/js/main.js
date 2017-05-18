817e391a53bc0c010824ef66e898f586b2aeb560
Yangfan <15234408101@163.com>
2017-05-18 21:15:26 +0800

    Update main.js

--- AMap-Vue/js/main.js
+++ AMap-Vue/js/main.js
@@ -1241,7 +1241,7 @@ var vm=new Vue({
 				if (!localStorage.getItem("bounds")) {
 					console.log("网络行政边界数据");
 					$.ajax({
-						url:"../TEST007/json/boundaries.json",
+						url:"../AMap-Vue/json/boundaries.json",
 						method:"GET",
 						success:function (xrh,status,result) {
 							var bounds=result.responseJSON.boundaries;
@@ -1260,7 +1260,7 @@ var vm=new Vue({
 				if (!localStorage.getItem("busLineData")) {
 					console.info("网络所有公交线路数据");
 					$.ajax({
-						url:"../../json/busline.json",
+						url:"../AMap-Vue/json/busline.json",
 						method:"GET",
 						success:function (xhr,status,result) {
 							var busLineData=result.responseJSON;
