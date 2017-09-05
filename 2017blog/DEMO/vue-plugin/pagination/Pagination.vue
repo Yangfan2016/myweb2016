2ee7ad776abb56b3ef8ebf0b563f2130118903c6
Yangfan <15234408101@163.com>
2017-09-05 17:16:52 +0800

    Update Pagination.vue

--- 2017blog/DEMO/vue-plugin/pagination/Pagination.vue
+++ 2017blog/DEMO/vue-plugin/pagination/Pagination.vue
@@ -7,86 +7,102 @@
         <a href="javascript:void(0);" class="page-item" v-if="pageNum > pageOffset && pageNum - curPage >=pageOffset-1" v-on:click="changePage((curPage+pageOffset)>=pageNum?pageNum:(curPage+pageOffset))" v-bind:title="'后'+pageOffset+'页'">...</a>
         <a href="javascript:void(0);" class="page-item" v-bind:class="{active:curPage==pageNum}" v-on:click="changePage(pageNum)">{{pageNum}}</a>
         <a href="javascript:void(0);" class="page-next" v-bind:class="{disable:curPage==pageNum}" v-on:click="nextPage">下一页</a>
-    </div>
+		<div class="page-item" v-if="isJump"><span>跳转到</span><input type="number"  min="1" v-bind:max="pageNum" v-on:keypress.13="jumpPageByNum" /><span>页</span></div>
+	</div>
 </template>
 <script>
 export default {
     name:"pagination", 
-    props:{
-        pageNum:{
-            type:Number,
-            required:true
-        },
-        current:{
-            type:Number,
-            default:1
-        }
-    },
-    data:function () {
-        return {
-            curPage:this.current,
-            pageOffset:5,
-            pageSlide:2,
-            pageList:[]
-        };
-    },
-    watch:{
-        pageNum:function (val,oldVal) {
-            // 重新生成新分页
-            this.pageList = this.calcPageList(this.curPage);
-            // 重新赋值当前页
-            this.curPage=this.current;
-        }
-    },
-    methods:{
-        calcPageList: function (current) {
-            var pageList = [];
-            if (this.pageNum > this.pageOffset) {
-                var left = Math.max(this.pageSlide, current - this.pageSlide);
-                var right = Math.min(current + this.pageSlide, this.pageNum - 1);
-                if (current - 1 < this.pageSlide) {
-                    right = this.pageOffset - 1;
-                }
-                if (this.pageNum - current < this.pageSlide) {
-                    left = this.pageNum - this.pageSlide - 1;
-                }
-                for (var i = left; i <= right; i++) {
-                    pageList.push(i);
-                }
-            } else {
-                for (var i = this.pageSlide; i < this.pageNum; i++) {
-                    pageList.push(i);
-                }
-            }
-            return pageList;
-        },
-        changePage: function (index) {
-            this.curPage = index;
-            // 重新生成新分页
-            this.pageList = this.calcPageList(this.curPage);
-            // 触发父组件相关事件
-            this.$emit("cur-page-change",this.curPage);
-        },
-        prevPage: function () {
-            if (this.curPage != 1) {
-                this.curPage--;
-                this.changePage(this.curPage);
-            }
-        },
-        nextPage: function () {
-            if (this.curPage != this.pageNum) {
-                this.curPage++;
-                this.changePage(this.curPage);
-            }
-        },
-    },
-    mounted:function () {
-        this.$nextTick(function () {
-            // init
-            // 生成分页
-            this.pageList=this.calcPageList(this.curPage);
-        });
-    }
+        props:{
+			pageNum:{
+				type:Number,
+				required:true
+			},
+			current:{
+				type:Number,
+				default:1
+			},
+			isJump:{
+				type:Boolean,
+				default:false
+			}
+		},
+		data:function () {
+			return {
+				curPage:this.current,
+				pageOffset:5,
+				pageSlide:2,
+				pageList:[]
+			};
+		},
+		watch:{
+			pageNum:function (val,oldVal) {
+				// 重新生成新分页
+				this.pageList = this.calcPageList(this.curPage);
+				// 重新赋值当前页
+				this.curPage=this.current;
+			},
+			current:function (val,oldVal) {
+				this.curPage=val;
+				// 重新生成新分页
+				this.pageList = this.calcPageList(val);
+			},
+			curPage:function (val,oldVal) {
+				// 重新生成新分页
+				this.pageList = this.calcPageList(val);
+				// 触发父组件相关事件
+				this.$emit("cur-page-change",val);
+			}
+		},
+		methods:{
+			calcPageList: function (current) {
+				var pageList = [];
+				if (this.pageNum > this.pageOffset) {
+					var left = Math.max(this.pageSlide, current - this.pageSlide);
+					var right = Math.min(current + this.pageSlide, this.pageNum - 1);
+					if (current - 1 < this.pageSlide) {
+						right = this.pageOffset - 1;
+					}
+					if (this.pageNum - current < this.pageSlide) {
+						left = this.pageNum - this.pageSlide - 1;
+					}
+					for (var i = left; i <= right; i++) {
+						pageList.push(i);
+					}
+				} else {
+					for (var i = this.pageSlide; i < this.pageNum; i++) {
+						pageList.push(i);
+					}
+				}
+				return pageList;
+			},
+			changePage: function (index) {
+				this.curPage = index;
+			},
+			prevPage: function () {
+				if (this.curPage != 1) {
+					this.curPage--;
+					this.changePage(this.curPage);
+				}
+			},
+			nextPage: function () {
+				if (this.curPage != this.pageNum) {
+					this.curPage++;
+					this.changePage(this.curPage);
+				}
+			},
+			jumpPageByNum:function (ev) {
+				this.curPage=Math.max(Math.min(this.pageNum,Number(ev.target.value)),1);
+                ev.target.value=this.curPage;
+			}
+		},
+		mounted:function () {
+			this.$nextTick(function () {
+				// init
+				// 生成分页
+				this.pageList=this.calcPageList(this.curPage);
+			});
+		},
 }
 </script>
 <style scoped>
